414e35118ab650d73f743d53e85bcb18b419a028
Robert Walter <robert@realsynth.de>
2019-02-21 14:31:07 +0900

    expose camera settings ro ROS via service calls

--- scripts/ip_webcam_bridge.py
+++ scripts/ip_webcam_bridge.py
@@ -2,15 +2,6 @@
 # Robert Walter
 #   https://github.com/robbeofficial
 
-import urllib2
-
-#import cv2
-#import numpy as np
-
-import rospy
-#from std_msgs.msg import String
-from sensor_msgs.msg import CompressedImage
-
 # Run IP Webcam on your android phone
 #  https://play.google.com/store/apps/details?id=com.pas.webcam&hl=de
 #  use it's browser interface to configure
@@ -18,24 +9,82 @@ from sensor_msgs.msg import CompressedImage
 # TODO allow changing of parameters through ROS messages / dynamic reconf
 # publish current camera settings as ros message
 # TODO create class
+# TODO doc: did not use dynreconf because too static
+# TODO check if it's okay to use msg.String instead of srv.GetStringResponse
+# TODO logging instead of print
+
+import urllib2
+import requests
+
+#import cv2
+#import numpy as np
+
+import rospy
+import std_msgs.msg
+import sensor_msgs.msg
+
+import android_sensor_bridge.srv
+
+def setting_get(setting):
+    status = requests.get(hoststr + '/status.json').json()
+    return std_msgs.msg.String(status['curvals'][setting])
+
+def setting_options(setting):
+    status = requests.get(hoststr + '/status.json', params = {'show_avail': 1}).json()
+    return std_msgs.msg.String(';'.join(status['avail'][setting])) # TODO catch key error
+
+def setting_set(setting, req):
+    # torch: /enabletorch /disabletorch
+    # focus: /focus /nofocus
+    # video_size: /settings/video_size?set=352x288
+
+    value = req.value.data
+    
+    # create a list of possible entry points
+    entry_points = [hoststr + '/settings/' + setting + "?set=" + value]
+    if value in ['on', 'off']:
+        entry_points.append(hoststr + ('enable' if value == 'on' else 'disable') + setting)
+        entry_points.append(hoststr + ('' if value == 'on' else 'no') + setting)
+    
+    # try calling them
+    for entry_point in entry_points:
+        print('querying ' + entry_point)
+        http = requests.get(entry_point)
+        if http.status_code == 200:
+            break
+
+    resp = http.text
+    return std_msgs.msg.String(resp) 
+    
+def add_settings_services():
+    settings = requests.get(hoststr + '/status.json').json()['curvals'].keys()
+    
+    for setting in settings:
+        if setting != " ":
+            rospy.Service("~" + setting + "/get", android_sensor_bridge.srv.GetString, 
+                lambda req, setting=setting : setting_get(setting)) 
+            rospy.Service("~" + setting + "/options", android_sensor_bridge.srv.GetString, 
+                lambda req, setting=setting : setting_options(setting))
+            rospy.Service("~" + setting + "/set", android_sensor_bridge.srv.SetString, 
+                lambda req, setting=setting : setting_set(setting, req)) 
 
 if __name__ == '__main__':
-
-    #print cv2.__version__
-
     rospy.init_node('ip_webcam_bridge')
-
-    # pubs
-    pub_image = rospy.Publisher('~image/compressed', CompressedImage, queue_size=1)
-
+ 
     # params
-    host = rospy.get_param('~host', '192.168.178.46')
+    host = rospy.get_param('~host', '192.168.43.1')
     port = rospy.get_param('~port', 8080)
 
-    # data stream
-    hoststr = 'http://{}:{}/video'.format(host, port)
-    rospy.loginfo('Streaming ' + hoststr)
-    stream = urllib2.urlopen(hoststr)
+    hoststr = 'http://{}:{}/'.format(host, port)
+
+    # pubs
+    pub_image = rospy.Publisher('~image/compressed', sensor_msgs.msg.CompressedImage, queue_size=1)
+
+    # services
+    add_settings_services()
+
+    rospy.loginfo('Streaming ' + hoststr + '/video')
+    stream = urllib2.urlopen(hoststr + '/video')
 
     buffer = ''
     while not rospy.is_shutdown():
@@ -47,7 +96,7 @@ if __name__ == '__main__':
             buffer = buffer[b+2:]
 
             # create and send CompressedImage message
-            img_msg = CompressedImage()
+            img_msg = sensor_msgs.msg.CompressedImage()
             img_msg.data = jpg
             img_msg.format = 'jpeg'
             pub_image.publish(img_msg)
